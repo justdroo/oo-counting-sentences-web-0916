24de43d3aaaa94d40bd8548d1b1aead12cf23417
Flatiron School <flatironschool@Drew-2.local>
2016-09-29 15:25:35 -0400

    Complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  # def count_sentences
+  #   i = 0
+  #   self.split.each do |word|
+  #   if word.sentence? || word.question? || word.exclamation?
+  #     i += 1
+  #   end
+  # end
+  #   i
+  # end
+
+  # def count_sentences
+  #   self.split.each_with_object([]) do |word, sentence_arr|
+  #     if word.sentence? || word.question? || word.exclamation?
+  #       sentence_arr << word
+  #     end
+  #   end.count
+  # end
+
+  # OPTIMIZE :)
   def count_sentences
-
+    self.split(/[.?!]\s/).count
   end
+
 end
